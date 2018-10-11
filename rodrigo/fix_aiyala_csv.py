cd01ea829bee683084696a978cbb67124c30775b
Rodrigo Mariano <rodmariano13@gmail.com>
2018-10-10 22:37:06 -0300

    Using pandas to read CSV

--- rodrigo/fix_aiyala_csv.py
+++ rodrigo/fix_aiyala_csv.py
@@ -1,7 +1,86 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# roll back pip: python3 -m pip install --user --upgrade pip==9.0.3
+
+import pandas as pd 
+
+PATH_FILE_INPUT = "aiyala_levy_theaters_2017_07_10.csv"
+PATH_FILE_OUTPUT = "aiyala_levy_theaters_2017_07_10_output.csv"
+
+df = pd.read_csv(PATH_FILE_INPUT)
+
+# creating a blank series 
+address_series = pd.Series([])
+number_series = pd.Series([])
+
+#  df.loc[i, 'number'] = number
+
+for i in range(0, len(df)):
+    address_with_number = df["address"][i]
+
+    line = str(address_with_number).lower()
+    line = line.replace("r.", "rua", 1).replace("av.", "avenida", 1).replace("pç.", "praça", 1)
+
+    # avoid problem with nan values, change to white space
+    if line == "nan":
+        address, number = "", ""
+    else:
+        try:
+            # split the address with the number, using the first occurrence
+            address, number = line.split(",", 1)
+        except ValueError:
+            # if it is not possible to unpack the line, it means that there is not number
+            address, number = line, ""
+
+    # remove the extra spaces of the variables
+    address = address.strip()
+    number = number.strip()
+
+    # add the address and the number into the CSV
+    address_series[i] = address
+    number_series[i] = number
+
+# remove the old address column ...
+del df["address"]
+# ... and create the new ones
+df["address"] = address_series
+df["number"] = number_series
+
+# fill the NaN values with white space
+df = df.fillna("")
+
+# convert dataframe to CSV
+df.to_csv(PATH_FILE_OUTPUT, encoding='utf-8')
+
+
+#print(df.head(15))
+
+print(df[['address', 'number']].head(15))
+
+"""
+print("\n")
+
+print(df[['address', 'number']])
+"""
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 from csv import reader, writer
+
 
 PATH_FILE_INPUT = "aiyala_levy_theaters_2017_07_10.csv"
 PATH_FILE_OUTPUT = "aiyala_levy_theaters_2017_07_10_output.csv"
@@ -24,12 +103,15 @@ with open(PATH_FILE_INPUT, "r") as csv_input:
     new_CSV = []
     new_CSV.append(header)
 
-    len_header = len(header)
-    
     for row in read_CSV:
+        new_CSV.append(row)
+    
+    for row in new_CSV:
 
+        print("len(row): ", len(row), " - len(header): ", len(header))
         # remove the bad columns without header
         while len(row) > len(header):
+            print(row)
             row.pop()  # remove the last column
 
         # put the address in lower case and replace the strings
@@ -50,9 +132,8 @@ with open(PATH_FILE_INPUT, "r") as csv_input:
         # add the address and the number into the CSV
         row[index_address] = address
         row.append(number)
-
-        new_CSV.append(row)
-        #print(row, "\n")
+        
+        print(row, "\n")
 
     #for row in new_CSV:
      #   print(row, "\n")
@@ -61,5 +142,5 @@ with open(PATH_FILE_INPUT, "r") as csv_input:
     with open(PATH_FILE_OUTPUT, "w") as csv_output:
         writer = writer(csv_output, lineterminator='\n')
         writer.writerows(new_CSV)
-    
+'''    
 
